9959793c9b23f3b7d62b0cfd22dbf7d6496bc33e
Corry Frydlewicz <cfrydlewicz@gmail.com>
2022-01-05 02:32:42 -0800

    Add Header Nav Class

--- header.php
+++ header.php
@@ -40,7 +40,9 @@
 				<a href="#skip-to-content">Skip to content &gt;</a>
 			</section><!--.screen-reader-nav-->
 
-			<section><?php wp_nav_menu('primary'); ?></section>
+			<section class="header-nav">
+				<?php wp_nav_menu('primary'); ?>
+			</section>
 
 			<section class="header-search">
 				<div class="search-container icon-search"><?php get_search_form(); ?></div>
