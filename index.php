8c5b48b4868b96e80bf08be7fecfef1147eaeed5
Corry Frydlewicz <cfrydlewicz@gmail.com>
2022-01-04 16:48:22 -0800

    Basic Functions and Loop

--- index.php
+++ index.php
@@ -1,45 +1,61 @@
-<?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package WordPress
- * @subpackage Twenty_Twenty_One
- * @since Twenty Twenty-One 1.0
- */
-
-get_header(); ?>
+<?php get_header(); ?>
 
 <?php if ( is_home() && ! is_front_page() && ! empty( single_post_title( '', false ) ) ) : ?>
-	<header class="page-header alignwide">
+	<header class="page-header">
 		<h1 class="page-title"><?php single_post_title(); ?></h1>
-	</header><!-- .page-header -->
+	</header>
 <?php endif; ?>
 
-<?php
-if ( have_posts() ) {
+<main id="skip-to-content">
+<?php if ( have_posts() ) : ?>
+	<?php while ( have_posts() ) : ?>
+		<?php the_post(); ?>
 
-	// Load posts loop.
-	while ( have_posts() ) {
-		the_post();
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-		get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) );
-	}
+      <header class="entry-header">
+        <?php post_thumbnail(); ?>
+        <div class="thumbnail-overlay">
+          <?php if ( is_singular() ) : ?>
+            <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+          <?php else : ?>
+            <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+          <?php endif; ?>
 
-	// Previous/next page navigation.
-	twenty_twenty_one_the_posts_navigation();
+          <section class="post-meta">
+            <div class="post-stats"><span class="word-count"><?php word_count(); ?> words</span> posted on <span class="post-date"></span></div>
+            <div class="post-categories"><?php get_the_category_list(); ?></div>
+          </section>
 
-} else {
+        </div>
+      </header>
 
-	// If no content, include the "No posts found" template.
-	get_template_part( 'template-parts/content/content-none' );
+      <div class="entry-content">
+        <?php the_excerpt(); ?>
+      </div><!--.entry-content-->
 
-}
+      <footer class="entry-footer">
+        <a href="<?php esc_url(get_permalink()); ?>"><button>Read it Now!</button></a>
+      </footer>
+
+    </article>
+
+  <?php endwhile; ?>
+  <?php	posts_nav_link(); ?>
+
+<?php else : ?>
+
+  <section class="post error404">
+    <img src="<?php bloginfo('template_url');?>/assets/static/404.jpg">
+    <div class="entry-content">
+      <h2 class="entry-title">No Results Found</h2>
+      <div class="entry-summary">
+        <p>Sorry! Please <a href="#search">try another search</a> or <a href="/">return to the home page</a>.</p>
+      </div><!-- .entry-summary -->
+    </div><!--.text-container-->
+  </section>
+
+<?php endif; ?>
+</main>
 
 get_footer();
