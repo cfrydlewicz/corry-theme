fd2b7d75e62234e05764f2c8f6e01324bb18f1e5
Corry Frydlewicz <corry_frydlewicz@gap.com>
2022-01-04 16:50:56 -0800

    Index Fixes

--- index.php
+++ index.php
@@ -7,55 +7,55 @@
 <?php endif; ?>
 
 <main id="skip-to-content">
-<?php if ( have_posts() ) : ?>
-	<?php while ( have_posts() ) : ?>
-		<?php the_post(); ?>
+  <?php if ( have_posts() ) : ?>
+  	<?php while ( have_posts() ) : ?>
+  		<?php the_post(); ?>
 
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-      <header class="entry-header">
-        <?php post_thumbnail(); ?>
-        <div class="thumbnail-overlay">
-          <?php if ( is_singular() ) : ?>
-            <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-          <?php else : ?>
-            <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-          <?php endif; ?>
+        <header class="entry-header">
+          <?php post_thumbnail(); ?>
+          <div class="thumbnail-overlay">
+            <?php if ( is_singular() ) : ?>
+              <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+            <?php else : ?>
+              <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+            <?php endif; ?>
 
-          <section class="post-meta">
-            <div class="post-stats"><span class="word-count"><?php word_count(); ?> words</span> posted on <span class="post-date"></span></div>
-            <div class="post-categories"><?php get_the_category_list(); ?></div>
-          </section>
+            <section class="post-meta">
+              <div class="post-stats"><span class="word-count"><?php word_count(); ?> words</span> posted on <span class="post-date"></span></div>
+              <div class="post-categories"><?php get_the_category_list(); ?></div>
+            </section>
 
-        </div>
-      </header>
+          </div>
+        </header>
 
+        <div class="entry-content">
+          <?php the_excerpt(); ?>
+        </div><!--.entry-content-->
+
+        <footer class="entry-footer">
+          <a href="<?php esc_url(get_permalink()); ?>"><button>Read it Now!</button></a>
+        </footer>
+
+      </article>
+
+    <?php endwhile; ?>
+    <?php	posts_nav_link(); ?>
+
+  <?php else : ?>
+
+    <section class="post error404">
+      <img src="<?php bloginfo('template_url');?>/assets/static/404.jpg">
       <div class="entry-content">
-        <?php the_excerpt(); ?>
-      </div><!--.entry-content-->
+        <h2 class="entry-title">No Results Found</h2>
+        <div class="entry-summary">
+          <p>Sorry! Please <a href="#search">try another search</a> or <a href="/">return to the home page</a>.</p>
+        </div><!-- .entry-summary -->
+      </div><!--.text-container-->
+    </section>
 
-      <footer class="entry-footer">
-        <a href="<?php esc_url(get_permalink()); ?>"><button>Read it Now!</button></a>
-      </footer>
-
-    </article>
-
-  <?php endwhile; ?>
-  <?php	posts_nav_link(); ?>
-
-<?php else : ?>
-
-  <section class="post error404">
-    <img src="<?php bloginfo('template_url');?>/assets/static/404.jpg">
-    <div class="entry-content">
-      <h2 class="entry-title">No Results Found</h2>
-      <div class="entry-summary">
-        <p>Sorry! Please <a href="#search">try another search</a> or <a href="/">return to the home page</a>.</p>
-      </div><!-- .entry-summary -->
-    </div><!--.text-container-->
-  </section>
-
-<?php endif; ?>
+  <?php endif; ?>
 </main>
 
-get_footer();
+<?php get_footer(); ?>
