47f9e92f5388a8a6415eef05bb05300b2be6295a
Corry Frydlewicz <cfrydlewicz@gmail.com>
2022-01-06 13:22:22 -0800

    Layout Work

--- single.php
+++ single.php
@@ -5,8 +5,10 @@
     <?php while ( have_posts() ) : ?>
       <?php
         the_post();
+        // store data for the post footer widgets
         $postId = get_the_ID();
         $postTitle = get_the_title();
+        $postShortUrl = "https://corry.us/?p=".$postId;
       ?>
 
       <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -14,11 +16,13 @@
           <header class="entry-header">
             <?php post_thumbnail(); ?>
             <div class="thumbnail-overlay">
-              <h1 class="entry-title"><?php the_title(); ?></h1>
-
-              <section class="post-meta">
-              </section>
-
+              <div>
+                <h1 class="entry-title"><?php the_title(); ?></h1>
+                <section class="post-meta">
+                  <div class="post-stats"><span class="word-count"><?php word_count(); ?> words</span> published on <span class="post-date"><?php the_date(); ?></span></div>
+                  <div class="post-categories"><?php the_category(); ?></div>
+                </section>
+              </div>
             </div>
           </header>
 
@@ -27,66 +31,76 @@
           </section>
 
           <footer class="entry-footer">
-            <div class="post-categories">Categories: <?php the_category(', '); ?></div>
-            <div class="post-tags"><?php the_tags(); ?></div>
+            <p class="post-categories">Categories: <?php the_category(', '); ?></p>
+            <p class="post-tags"><?php the_tags(); ?></p>
           </footer>
         </div>
       </article>
 
-      <section class="post-footer">
+      <footer class="post-footer t_slides-up">
         <div class="inner-wrapper--at-md">
 
-        <div class="thanks-container">
-          <div class="f_large">Thanks for reading!</div>
-        </div>
+          <section class="thanks-container">
+            <div class="f_large">Thanks for reading!</div>
+          </section>
 
-        <div class="discussion-container">
-          <div class="post-footer_header">Discuss</div>
-          <ul>
-            <?php if ( comments_open() ) : ?>
-              <?php if ( get_comments_number() ) : ?>
-                <li><a class="i_arrow-down" href="#a_comments_top">View Comments (<?php echo get_comments_number(); ?>)</a></li>
+          <section class="discussion-container">
+            <div class="post-footer_header">Discuss</div>
+            <ul>
+              <?php if ( comments_open() ) : ?>
+                <?php if ( get_comments_number() ) : ?>
+                  <li><a class="i_arrow-down" href="#a_comments_top">View Comments (<?php echo get_comments_number(); ?>)</a></li>
+                <?php endif; ?>
+                <li><a class="i_chat" href="#a_reply-title">Leave a Comment</a></li>
               <?php endif; ?>
-              <li><a class="i_chat" href="#a_reply-title">Leave a Comment</a></li>
+              <li><a class="i_twitter" href="https://twitter.com/intent/tweet?text=%40cfrydlewicz%20Re%3A%20https://corry.us/?p=<?php echo $postId; ?>" target="_blank">Discuss on Twitter</a>
+              <li><a class="i_facebook" href="https://www.facebook.com/groups/4912504758808432/?ref=share" target="_blank">Facebook Group</a></li>
+            </ul>
+          </section>
+
+          <section class="share-container">
+            <div class="post-footer_header">Share</div>
+            <ul>
+              <li>Short URL: <a href="<?php echo $postShortUrl; ?>" class="u_break-line"><?php echo $postShortUrl; ?></a></li>
+              <li><a class="i_mail" href="mailto:?subject=<?php echo $postTitle; ?>&body=<?php echo $postShortUrl; ?>" target="_blank">Send Email</a></li>
+              <li><a class="i_facebook" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $postShortUrl; ?>" target="_blank">Share on Facebook</a></li>
+              <li><a class="i_twitter" href="https://twitter.com/intent/tweet?text=%40cfrydlewicz%20Re%3A%20<?php echo $postShortUrl; ?>" target="_blank">Tweet This Post</a></li>
+            </ul>
+          </section>
+
+          <section class="read-more-container">
+            <?php if ( is_active_sidebar( 'widget_single-post-footer1' ) ) : ?>
+              <div class="widget-area" role="complementary">
+                <?php dynamic_sidebar( 'widget_single-post-footer1' ); ?>
+              </div>
+            <?php else : ?>
+              <div class="widget-title">Read More</div>
+              <div><a href="/tag/favorite-post/">My Favorite Posts</a></div>
             <?php endif; ?>
-            <li><a class="i_twitter" href="https://twitter.com/intent/tweet?text=%40cfrydlewicz%20Re%3A%20https://corry.us/?p=<?php echo $postId; ?>" target="_blank">Discuss on Twitter</a>
-            <li><a class="i_facebook" href="https://www.facebook.com/groups/4912504758808432/?ref=share" target="_blank">Facebook Group</a></li>
-          </ul>
-        </div>
+          </section>
 
-        <div class="share-container">
-          <div class="post-footer_header">Share</div>
-          <ul>
-            <li>Short URL: <a href="https://corry.us/?p=<?php echo $postId; ?>" class="u_break-line">https://corry.us/?p=<?php echo $postId; ?></a></li>
-            <li><a class="i_mail" href="mailto:?subject=<?php echo $postTitle; ?>&body=https%3A//corry.us/?p=<?php echo $postId; ?>" target="_blank">Send Email</a></li>
-            <li><a class="i_facebook" href="https://www.facebook.com/sharer/sharer.php?u=https%3A//corry.us/?p=<?php echo $postId; ?>" target="_blank">Share on Facebook</a></li>
-            <li><a class="i_twitter" href="https://twitter.com/intent/tweet?text=%40cfrydlewicz%20Re%3A%20https://corry.us/?p=<?php echo $postId; ?>" target="_blank">Tweet This Post</a></li>
-          </ul>
-        </div>
+          <section class="follow-container">
+            <div class="post-footer_header">Follow This Blog</div>
+            <ul>
+              <li><a class="i_rss" href="<?php bloginfo('rss2_url'); ?>" target="_blank">My RSS Feed</a> is the way to follow <em>only new articles</em>.</li>
+              <li><a class="i_facebook" href="https://www.facebook.com/groups/4912504758808432/?ref=share" target="_blank" rel="noreferrer" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','http://www.facebook.com']);">My Facebook Group</a> is where I share <em>all content I create</em> and host discussions about it.</li>
+              <li>If you don't have those, I share links to new posts on <a class="i_twitter"  href="http://www.twitter.com/cfrydlewicz" target="_blank" rel="noreferrer" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','http://www.twitter.com']);">Twitter</a> as well.</li>
+            </ul>
+          </section>
 
-        <div class="read-more-container">
-          <?php if ( is_active_sidebar( 'widget_single-post-footer1' ) ) : ?>
-            <div class="widget-area" role="complementary">
-              <?php dynamic_sidebar( 'widget_single-post-footer1' ); ?>
-            </div>
-          <?php else : ?>
-            <div class="widget-title">Read More</div>
-            <div><a href="/tag/favorite-post/">My Favorite Posts</a></div>
+          <?php if ( comments_open() ) : ?>
+            <section id="a_comments_top" class="comments-container t_slides-up">
+              <div class="post-footer_header">Comments</div>
+              <?php
+                if ( get_comments_number() ) {
+                  comments_template();
+                }
+              ?>
+            </section>
           <?php endif; ?>
+
         </div>
-
-        <?php if ( comments_open() ) : ?>
-          <div id="a_comments_top" class="comments-container">
-            <div class="post-footer_header">Comments</div>
-            <?php
-              if ( get_comments_number() ) {
-                comments_template();
-              }
-            ?>
-          </div>
-        <?php endif; ?>
-
-      </section>
+      </footer>
 
     <?php endwhile; ?>
   <?php else : ?>
