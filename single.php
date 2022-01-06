4be04dc5401660bbc36cf1af1a92ebac6147aac2
Corry Frydlewicz <cfrydlewicz@gmail.com>
2022-01-06 02:22:01 -0800

    Interactive Elements

--- single.php
+++ single.php
@@ -27,8 +27,8 @@
           </section>
 
           <footer class="entry-footer">
-            <div class="post-categories">Categorized: <?php the_category(); ?></div>
-            <div class="post-tags">Tagged: <?php the_tags(); ?></div>
+            <div class="post-categories">Categories: <?php the_category(); ?></div>
+            <div class="post-tags"><?php the_tags(); ?></div>
           </footer>
         </div>
       </article>
