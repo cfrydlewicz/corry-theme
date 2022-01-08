4de82131c0cf33a15244f92cb61ca2d5d011542c
Corry Frydlewicz <cfrydlewicz@gmail.com>
2022-01-07 17:37:15 -0800

    Commit to Newer Method

--- dev/js/custom.js
+++ dev/js/custom.js
@@ -4,38 +4,15 @@
 // Secondary site header
 // For single posts
 $('#single-post-title').ready( function() {
+  // Watch the h1
   var observer = new IntersectionObserver(function(entries) {
     if(entries[0].isIntersecting === true) {
+      // hide the secondary header when h1 is visible
       $('#site-header-secondary').slideUp();
     } else {
+      // show the secondary header when h1 isn't visible
       $('#site-header-secondary').slideDown();
     }
   }, { threshold: [0] });
   observer.observe(document.querySelector("#single-post-title"));
 });
-
-/*
-// For single posts
-$('#single-post-title').ready( function() {
-
-  // as user scrolls
-  window.addEventListener('scroll', function() {
-    // keep track of the h1 and its shape
-    var element = document.querySelector('#single-post-title');
-    var position = element.getBoundingClientRect();
-
-    // when it is visible
-    if ( position.top >= 0 && position.bottom <= window.innerHeight ) {
-      // Hide the secondary fixed header
-      $('#site-header-secondary').slideUp();
-
-    // when it is not visible
-    } else {
-      // Show the secondary fixed header
-      $('#site-header-secondary').slideDown();
-    }
-
-  });
-
-});
-*/
