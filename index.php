c8d335641e9e5feee02e6ce28f3bf732b8db698d
Corry Frydlewicz <cfrydlewicz@gmail.com>
2022-01-08 12:24:51 -0800

    Non-home Listing Pages

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php get_header(); ?>
 
-<?php if ( is_home() && ! is_front_page() && ! empty( single_post_title( '', false ) ) ) : ?>
-  <header class="page-header">
-    <h1 class="page-title"><?php single_post_title(); ?></h1>
-  </header>
-<?php endif; ?>
 
 <main id="a_skip-to-content">
+
+  <?php if ( ! is_home() ) : ?>
+    <header class="page-header">
+      <h1 class="page-title"><?php the_title(); ?></h1>
+    </header>
+  <?php endif; ?>
+
   <?php if ( have_posts() ) : ?>
     <?php while ( have_posts() ) : ?>
       <?php the_post(); ?>
