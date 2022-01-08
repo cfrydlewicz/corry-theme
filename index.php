bdc0a824dcd9ee878b32f7fa1e6ed65f63fa9007
Corry Frydlewicz <cfrydlewicz@gmail.com>
2022-01-08 10:09:31 -0800

    Article Cards Markup

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   </header>
 <?php endif; ?>
 
-<main id="a_skip-to-content" class="inner-wrapper">
+<main id="a_skip-to-content">
   <?php if ( have_posts() ) : ?>
     <?php while ( have_posts() ) : ?>
       <?php the_post(); ?>
@@ -18,24 +18,28 @@
             <?php post_thumbnail(); ?>
             <div class="title-container thumbnail-overlay">
               <?php if ( is_singular() ) : ?>
-                <h1 class="entry-title"><?php the_title(); ?></h1>
+                <h1 id="single-post-title" class="entry-title"><?php the_title(); ?></h1>
               <?php else : ?>
                 <h2 class="entry-title"><?php the_title( sprintf( '<a href="%s">', esc_url( get_permalink() ) ), '</a>' ); ?></h2>
               <?php endif; ?>
-            </div>
-            <div class="post-meta thumbnail-overlay">
-              <div class="post-stats"><span class="word-count"><?php word_count(); ?> words</span> published on <span class="post-date"><?php the_date(); ?></span></div>
-              <div class="post-categories"><?php the_category(); ?></div>
+              <div class="post-meta">
+                <div class="post-stats"><span class="word-count"><?php word_count(); ?> words</span> published on <span class="post-date"><?php the_date(); ?></span></div>
+                <div class="post-categories"><?php the_category(); ?></div>
+              </div>
             </div>
           </div>
         </header>
 
         <div class="entry-content">
-          <?php the_excerpt(); ?>
+          <div class="inner-wrapper">
+            <?php the_excerpt(); ?>
+          </div>
         </div><!--.entry-content-->
 
         <footer class="entry-footer">
-          <a href="<?php echo str_replace(home_url(), '', get_permalink()); ?>" class="button">Read it Now!</a>
+          <div class="inner-wrapper">
+            <a href="<?php echo str_replace(home_url(), '', get_permalink()); ?>" class="button">Read it Now!</a>
+          </div>
         </footer>
 
       </article>
@@ -45,14 +49,24 @@
 
   <?php else : ?>
 
-    <section class="post error404">
-      <img src="<?php bloginfo('template_url');?>/assets/404.jpg">
+    <article id="post error404">
+
+      <header class="entry-header">
+        <div class="inner-wrapper--at-md">
+          <img src="<?php bloginfo('template_url');?>/assets/404.jpg">
+          <div class="title-container thumbnail-overlay">
+            <h2 class="entry-title">No Results Found</h2>
+          </div>
+        </div>
+      </header>
+
       <div class="entry-content">
         <div class="inner-wrapper">
           <p>Sorry! Please <a href="#a_search" onclick="$('#header-search-field').focus();">try another search</a> or <a href="/">return to the home page</a>.</p>
         </div>
       </div><!--.entry-content-->
-    </section>
+
+    </article>
 
   <?php endif; ?>
 </main>
