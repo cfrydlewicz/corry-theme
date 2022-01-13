0293b89e1ff2ce0788446c946b58f32c1226e494
Corry Frydlewicz <cfrydlewicz@gmail.com>
2022-01-12 17:26:56 -0800

    Remove Arimo
    Commit to Montserrat. Remove unused weights after adjusting.

--- header.php
+++ header.php
@@ -67,9 +67,10 @@
 
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Arimo:ital,wght@0,400;0,500;0,700;1,400;1,700&family=Montserrat:wght@100;200;300;400&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+  <!--link href="https://fonts.googleapis.com/css2?family=Arimo:ital,wght@0,400;0,500;0,700;1,400;1,700&family=Montserrat:wght@100;200;300;400&display=swap" rel="stylesheet"-->
 
-  <style>/* critical css */body{font-family:Arimo,helvetica,sans-serif}</style>
+  <style>/* critical css */</style>
   <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url');?>/style.css">
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
