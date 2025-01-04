a8787534fc7736341512f71c2978cfacef471665
Corry Frydlewicz <cfrydlewicz@gmail.com>
2025-01-04 15:18:48 -0800

    Attempt to Fix iPhone Page Width

--- dev/header.php
+++ dev/header.php
@@ -4,7 +4,7 @@ $saveData = (isset($_SERVER["HTTP_SAVE_DATA"]) && stristr($_SERVER["HTTP_SAVE_DA
 <html lang="en" class="<?php if ($saveData === true) : ?>save-data<?php endif; ?>">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
   <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
