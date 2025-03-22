e3607e5d3487c3dac6232b4b46593da12a1aceac
Corry Frydlewicz <cfrydlewicz@gmail.com>
2025-03-22 14:56:30 -0700

    Update Old Posts and Microblog Notices

--- single.php
+++ single.php
@@ -37,16 +37,8 @@
           <?php if ( has_tag('old-post') ) : ?>
             <div class="disclaimer_old-posts disclaimer">
               <div>
-                <p><strong>This post is old and contains outdated information.</strong> I make a point to <em>only remove harmful content</em>, and I try to keep old posts visible &mdash; even if they're embarrassing or no longer accurate. I want to look back and see my growth over the years, and I don't want to hide that growth from others either. Thanks for considering this as you read.</p>
-                <p>If you'd like to see an update for this content, <a href="#a_respond" class="i_chat">leave a comment</a> saying so.</p>
-              </div>
-            </div>
-          <?php endif; ?>
-
-          <?php if ( has_tag('microblog') ) : ?>
-            <div class="disclaimer_microblog disclaimer">
-              <div>
-                <p>This post is a <a href="/microblogging/">microblog</a>, which is an experimental new feature.</p>
+                <p><strong>This post is old and contains outdated information.</strong> I <em>only remove harmful content</em> and try to keep old posts visible &mdash; even if they're embarrassing or no longer relevant. I want to look back and see my growth over the years, and I don't want to hide that growth from others. Thanks for considering this as you read.</p>
+                <p>(<a href="#a_respond" class="i_chat">Leave a comment</a> if you want an update about this subject matter)</p>
               </div>
             </div>
           <?php endif; ?>
