8e67cb81ceaaf1352e9960e4cbe1e475b39696e3
Corry Frydlewicz <cfrydlewicz@gmail.com>
2025-03-28 12:56:01 -0700

    Minimal Read More Widget on Pages
    And remove Post Footer Template Part

--- page.php
+++ page.php
@@ -44,7 +44,47 @@
 
     </article>
 
-    <?php get_template_part('template-parts/post-footer', '', $thisPostId) ?>
+    <?php
+      echo $thisPostId[0];
+      $thisPostTitle = get_the_title($thisPostId);
+      $thisPostShortUrl = "https://corry.us/?p=".get_the_ID($thisPostId);
+      $thisPostCommentNum = get_comments_number($thisPostId);
+    ?>
+
+    <footer class="post-footer t_slides-up">
+
+      <?php if ( !empty($thisPostShortUrl) ) : ?>
+        <section class="share-container">
+          <div class="post-footer_header">Share</div>
+          <ul>
+            <li><a class="i_mail" href="mailto:%20?subject=<?php echo $thisPostTitle; ?>&body=<?php echo $thisPostShortUrl; ?>" target="_blank">Share via Email</a></li>
+            <li><a class="i_bluesky" href="https://bsky.app/intent/compose?text=<?php echo $thisPostShortUrl; ?>" target="_blank" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://bsky.app']);">Share on Bluesky</a></li>
+            <li>URL: <a href="https://corry.us/<?php echo $post->post_name; ?>/" class="f_small">https://corry.us/<?php echo $post->post_name; ?>/</a></li>
+          </ul>
+        </section>
+      <?php endif; ?>
+
+      <section class="follow-container">
+        <div class="post-footer_header">Follow Me</div>
+        <ul>
+          <li><a class="i_mail" href="/emailsubscribe?utm_source=post-footer" target="_blank">Email me</a> at <a href="mailto:cfrydlewicz@gmail.com" target="_blank">cfrydlewicz@gmail.com</a> to be notified when I post a new article.</li>
+          <li><a class="i_rss" href="/subscribe-to-corrys-blog-with-rss/" target="_blank">My RSS Feed</a> is the next best way to follow <em>new blog posts</em>.</li>
+          <li>I also share them on <a class="i_bluesky" href="/bluesky" rel="noreferrer" target="_blank" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://bsky.app']);">Bluesky</a>.</li>
+          <!--li><a class="i_heart" href="https://www.patreon.com/CorryFrydlewicz" target="_blank" rel="noreferrer" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://www.patreon.com']);">Patreon</a> is where I'll share more once I have a few subscribers.</li-->
+        </ul>
+      </section>
+
+      <section class="read-more-container">
+        <div class="post-footer_header">Read More</div>
+        <ul>
+          <li><a href="/about/">About Me</a></li>
+          <li><a href="/tag/favorite-post/">My Favorite Posts</a></li>
+          <li><a href="/">All Blog Posts</a></li>
+          <li><a href="/category/microblog/">Microblog</a></li>
+        </ul>
+      </section>
+
+    </footer>
 
   <?php endwhile; ?>
 </main>
