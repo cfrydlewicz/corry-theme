ddebeae90f9f756e4da2eb4b463f932e3f251880
Corry Frydlewicz <cfrydlewicz@gmail.com>
2025-05-22 12:00:38 -0700

    Update 404 Page

--- index.php
+++ index.php
@@ -78,26 +78,38 @@
   <?php else : ?>
 
     <article id="error404" class="article-card">
-
       <header class="entry-header">
-        <picture>
-          <source media="(min-width: 769px)" srcset="<?php bloginfo('template_url');?>/assets/images/404_1024x512.jpg">
-          <img src="<?php bloginfo('template_url');?>/assets/images/404_512x512.jpg" alt="Mega Man enters a boss stage only to find a 404 Boss Not found instead">
-        </picture>
-        <div class="title-container">
-          <h1 class="entry-title">No Results Found</h1>
+        <div class="stretched-bg" style="background-image: url('<?php bloginfo('template_url');?>/assets/images/404_1024x512.jpg');"></div>
+        <div class="inner-wrapper--at-lg" data-thumbnail="<?php the_post_thumbnail_url(); ?>">
+          <a href="/">
+            <picture>
+              <source media="(min-width: 769px)" srcset="<?php bloginfo('template_url');?>/assets/images/404_1024x512.jpg">
+              <img src="<?php bloginfo('template_url');?>/assets/images/404_512x512.jpg" alt="Mega Man enters a boss stage only to find a 404 Boss Not found instead">
+            </picture>
+          </a>
+          <div class="title-container">
+            <?php if ( is_search() ) : ?>
+              <h1 id="sticky-title" class="entry-title">Sorry, No Results Found</h1>
+            <?php else : ?>
+              <h2 class="entry-title"><a href="/">Oops! Wrong turn!</a></h2>
+            <?php endif; ?>
+          </div>
         </div>
       </header>
-
       <div class="entry-content">
         <div class="inner-wrapper">
           <div class="excerpt-container">
-            <p>I couldn't find what you're looking for. Please try another search or <span style="white-space: nowrap;">return to the ...</span></p>
-            <a href="/" class="i_arrow-right--after">Homepage</a>
+            <p>I couldn't find what you're looking for. Please try searching in another way or check out my homepage. If you think something's broken and have a minute to <a href="mailto:cfrydlewicz@gmail.com">let me know</a>, I'd appreciate it.</p>
+            <a href="/" class="i_arrow-right--after">Latest Posts</a>
           </div>
         </div>
       </div><!--.entry-content-->
-
+      <footer class="entry-footer">
+        <div class="inner-wrapper">
+          <div class="post-stats"><span class="word-count">0 words</span> <span class="published-on">published in</span> <span class="post-date">20XX</span></div>
+          <div class="post-categories"><span class="u_visually-hidden">Categories:&nbsp;</span>404 Page</div>
+        </div>
+      </footer>
     </article>
 
   <?php endif; ?>
