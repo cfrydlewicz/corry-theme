03ca6c2a131c9b0fbfcb75dbc49e54a2a93e953b
Corry Frydlewicz <cfrydlewicz@gmail.com>
2025-05-30 10:47:56 -0700

    Prep for Sidebar Addition

--- index.php
+++ index.php
@@ -1,113 +1,115 @@
 <?php get_header(); ?>
 
-<main id="a_skip-to-content" class="listing-page">
+<?php if ( ! is_home() ) : ?>
+  <header class="listing-page-header">
+    <div class="inner-wrapper">
+      <?php if ( is_category() ) : ?>
+        <h1 id="sticky-title" class="page-title"><span>Category:</span> <strong><?php single_cat_title(); ?></strong></h1>
+        <?php echo category_description(); ?>
+      <?php elseif ( is_tag() ) : ?>
+        <h1 id="sticky-title" class="page-title"><span>Tag:</span> <strong><?php single_tag_title(); ?></strong></h1>
+      <?php elseif ( is_search() ) : ?>
+        <h1 id="sticky-title" class="page-title"><span>Search Results for</span> <strong><?php echo esc_html($_GET['s']); ?></strong></h1>
+      <?php endif; ?>
+    </div>
+  </header>
+<?php endif; ?>
 
-  <?php if ( ! is_home() ) : ?>
-    <header class="listing-page-header">
-      <div class="inner-wrapper">
-        <?php if ( is_category() ) : ?>
-          <h1 id="sticky-title" class="page-title"><span>Category:</span> <strong><?php single_cat_title(); ?></strong></h1>
-          <?php echo category_description(); ?>
-        <?php elseif ( is_tag() ) : ?>
-          <h1 id="sticky-title" class="page-title"><span>Tag:</span> <strong><?php single_tag_title(); ?></strong></h1>
-        <?php elseif ( is_search() ) : ?>
-          <h1 id="sticky-title" class="page-title"><span>Search Results for</span> <strong><?php echo esc_html($_GET['s']); ?></strong></h1>
-        <?php else : ?>
-          <div class="page-title"><strong>Oops!</strong></div>
-          <p>Sorry, I couldn't find what you were looking for. Try a search maybe?</p>
-        <?php endif; ?>
+<div class="main-columns-wrapper">
+
+  <main id="a_skip-to-content" class="listing-page">
+    <?php if ( have_posts() ) : ?>
+
+      <?php while ( have_posts() ) : ?>
+        <?php the_post(); ?>
+
+        <article id="post-<?php the_ID(); ?>" <?php post_class('article-card'); ?>>
+
+          <header class="entry-header">
+            <?php if ( has_post_thumbnail() ) : ?>
+              <div class="stretched-bg" style="background-image: url('<?php the_post_thumbnail_url(); ?>');"></div>
+            <?php endif;?>
+            <div class="inner-wrapper--at-lg">
+              <a href="<?php the_permalink(); ?>"><?php post_thumbnail(); ?></a>
+              <div class="title-container">
+                <?php if ( is_singular() ) : ?>
+                  <h1 id="sticky-title" class="entry-title"><?php the_title(); ?></h1>
+                <?php else : ?>
+                  <h2 class="entry-title"><?php the_title( sprintf( '<a href="%s">', esc_url( get_permalink() ) ), '</a>' ); ?></h2>
+                <?php endif; ?>
+              </div>
+            </div>
+          </header>
+
+          <div class="entry-content">
+            <div class="inner-wrapper">
+              <div class="excerpt-container">
+                <?php the_excerpt(); ?>
+                <a href="<?php echo str_replace(home_url(), '', get_permalink()); ?>" class="i_arrow-right--after">Read it Now!</a>
+              </div>
+            </div>
+          </div><!--.entry-content-->
+
+          <footer class="entry-footer">
+            <div class="inner-wrapper">
+              <div class="post-stats"><span class="post-date"><?php the_date(); ?></span> <span class="separator">|</span> <span class="word-count"><?php word_count(); ?> words</span></div>
+              <div class="post-categories"><span class="u_visually-hidden">Categories:&nbsp;</span><?php the_category(',&nbsp;'); ?></div>
+            </div>
+          </footer>
+
+        </article>
+
+      <?php endwhile; ?>
+
+      <div class="pagination-container">
+        <div class="inner-wrapper">
+          <?php posts_nav_link('', 'Previous Page', 'Next Page'); ?>
+        </div>
       </div>
-    </header>
-  <?php endif; ?>
 
-  <?php if ( have_posts() ) : ?>
+    <?php else : ?>
 
-    <?php while ( have_posts() ) : ?>
-      <?php the_post(); ?>
-
-      <article id="post-<?php the_ID(); ?>" <?php post_class('article-card'); ?>>
-
+      <article id="error404" class="article-card">
         <header class="entry-header">
-          <?php if ( has_post_thumbnail() ) : ?>
-            <div class="stretched-bg" style="background-image: url('<?php the_post_thumbnail_url(); ?>');"></div>
-          <?php endif;?>
+          <div class="stretched-bg" style="background-image: url('<?php bloginfo('template_url');?>/assets/images/404_1024x512.jpg');"></div>
           <div class="inner-wrapper--at-lg">
-            <a href="<?php the_permalink(); ?>"><?php post_thumbnail(); ?></a>
+            <a href="/">
+              <picture>
+                <source media="(min-width: 769px)" srcset="<?php bloginfo('template_url');?>/assets/images/404_1024x512.jpg">
+                <img src="<?php bloginfo('template_url');?>/assets/images/404_512x512.jpg" alt="Mega Man enters a boss stage only to find a 404 Boss Not found instead">
+              </picture>
+            </a>
             <div class="title-container">
-              <?php if ( is_singular() ) : ?>
-                <h1 id="sticky-title" class="entry-title"><?php the_title(); ?></h1>
+              <?php if ( is_search() ) : ?>
+                <h1 id="sticky-title" class="entry-title">Sorry, No Results Found</h1>
               <?php else : ?>
-                <h2 class="entry-title"><?php the_title( sprintf( '<a href="%s">', esc_url( get_permalink() ) ), '</a>' ); ?></h2>
+                <h2 class="entry-title"><a href="/">Oops! Wrong turn!</a></h2>
               <?php endif; ?>
             </div>
           </div>
         </header>
-
         <div class="entry-content">
           <div class="inner-wrapper">
             <div class="excerpt-container">
-              <?php the_excerpt(); ?>
-              <a href="<?php echo str_replace(home_url(), '', get_permalink()); ?>" class="i_arrow-right--after">Read it Now!</a>
+              <p>I couldn't find what you're looking for. Please try searching in another way or check out my homepage. If you think something's broken and have a minute to <a href="mailto:cfrydlewicz@gmail.com">let me know</a>, I'd appreciate it.</p>
+              <a href="/" class="i_arrow-right--after">Latest Posts</a>
             </div>
           </div>
         </div><!--.entry-content-->
-
         <footer class="entry-footer">
           <div class="inner-wrapper">
-            <div class="post-stats"><span class="post-date"><?php the_date(); ?></span> <span class="separator">|</span> <span class="word-count"><?php word_count(); ?> words</span></div>
-            <div class="post-categories"><span class="u_visually-hidden">Categories:&nbsp;</span><?php the_category(',&nbsp;'); ?></div>
+            <div class="post-stats"><span class="post-date">20XX</span> <span class="separator">|</span> <span class="word-count">0 words</span></div>
+            <div class="post-categories"><span class="u_visually-hidden">Categories:&nbsp;</span>404 Page</div>
           </div>
         </footer>
-
       </article>
 
-    <?php endwhile; ?>
+    <?php endif; ?>
 
-    <div class="pagination-container">
-      <div class="inner-wrapper">
-        <?php posts_nav_link('', 'Previous Page', 'Next Page'); ?>
-      </div>
-    </div>
+  </main>
 
-  <?php else : ?>
+  <aside class="sidebar">
+  </aside>
 
-    <article id="error404" class="article-card">
-      <header class="entry-header">
-        <div class="stretched-bg" style="background-image: url('<?php bloginfo('template_url');?>/assets/images/404_1024x512.jpg');"></div>
-        <div class="inner-wrapper--at-lg">
-          <a href="/">
-            <picture>
-              <source media="(min-width: 769px)" srcset="<?php bloginfo('template_url');?>/assets/images/404_1024x512.jpg">
-              <img src="<?php bloginfo('template_url');?>/assets/images/404_512x512.jpg" alt="Mega Man enters a boss stage only to find a 404 Boss Not found instead">
-            </picture>
-          </a>
-          <div class="title-container">
-            <?php if ( is_search() ) : ?>
-              <h1 id="sticky-title" class="entry-title">Sorry, No Results Found</h1>
-            <?php else : ?>
-              <h2 class="entry-title"><a href="/">Oops! Wrong turn!</a></h2>
-            <?php endif; ?>
-          </div>
-        </div>
-      </header>
-      <div class="entry-content">
-        <div class="inner-wrapper">
-          <div class="excerpt-container">
-            <p>I couldn't find what you're looking for. Please try searching in another way or check out my homepage. If you think something's broken and have a minute to <a href="mailto:cfrydlewicz@gmail.com">let me know</a>, I'd appreciate it.</p>
-            <a href="/" class="i_arrow-right--after">Latest Posts</a>
-          </div>
-        </div>
-      </div><!--.entry-content-->
-      <footer class="entry-footer">
-        <div class="inner-wrapper">
-          <div class="post-stats"><span class="post-date">20XX</span> <span class="separator">|</span> <span class="word-count">0 words</span></div>
-          <div class="post-categories"><span class="u_visually-hidden">Categories:&nbsp;</span>404 Page</div>
-        </div>
-      </footer>
-    </article>
-
-  <?php endif; ?>
-
-</main>
-
+</div><!--.main-columns-wrapper-->
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,138 +1,144 @@
 <?php get_header(); ?>
 
-<main id="a_skip-to-content"> 
-  <?php while ( have_posts() ) : ?>
-    <?php
-      the_post();
-      // store data for the post footer widgets
-      $thisPostId = get_the_ID();
-    ?>
+<div class="main-columns-wrapper">
 
-    <article id="post-<?php the_ID(); ?>" <?php post_class('article-content'); ?>>
+  <main id="a_skip-to-content"> 
+    <?php while ( have_posts() ) : ?>
+      <?php
+        the_post();
+        // store data for the post footer widgets
+        $thisPostId = get_the_ID();
+      ?>
 
-      <header class="entry-header">
-        <div class="stretched-bg" style="background-image: url('<?php 
-          if ( has_post_thumbnail() ) {
-            the_post_thumbnail_url();
-          } else if ( is_category(700) ) {
-            echo "/wp-content/themes/corry/assets/images/thumbnail-microblog.jpg";
-          } else {
-            echo "/wp-content/themes/corry/assets/images/thumbnail-default.jpg";
-          }
-        ?>');"></div>
-        <div class="inner-wrapper--at-lg">
-          <a href="<?php the_post_thumbnail_url(); ?>">
-            <?php post_thumbnail(); ?>
-          </a>
-          <div class="title-container">
-            <h1 id="sticky-title" class="entry-title"><?php the_title(); ?></h1>
-            <div class="post-meta"><span class="word-count"><?php word_count(); ?> words</span> published on <span class="post-date"><?php the_date(); ?></span></div>
+      <article id="post-<?php the_ID(); ?>" <?php post_class('article-content'); ?>>
+
+        <header class="entry-header">
+          <div class="stretched-bg" style="background-image: url('<?php 
+            if ( has_post_thumbnail() ) {
+              the_post_thumbnail_url();
+            } else if ( is_category(700) ) {
+              echo "/wp-content/themes/corry/assets/images/thumbnail-microblog.jpg";
+            } else {
+              echo "/wp-content/themes/corry/assets/images/thumbnail-default.jpg";
+            }
+          ?>');"></div>
+          <div class="inner-wrapper--at-lg">
+            <a href="<?php the_post_thumbnail_url(); ?>">
+              <?php post_thumbnail(); ?>
+            </a>
+            <div class="title-container">
+              <h1 id="sticky-title" class="entry-title"><?php the_title(); ?></h1>
+              <div class="post-meta"><span class="word-count"><?php word_count(); ?> words</span> published on <span class="post-date"><?php the_date(); ?></span></div>
+            </div>
           </div>
-        </div>
-      </header>
+        </header>
 
-      <section class="entry-content">
-        <div class="inner-wrapper">
+        <section class="entry-content">
+          <div class="inner-wrapper">
 
-          <?php if ( has_tag('old-post') ) : ?>
-            <div class="disclaimer_old-posts alert warning">
-              <div>
-                <p class="f_small"><strong>This post contains outdated information.</strong> I only remove <em>harmful</em> content and try to keep old posts visible, even if they're <em>embarrassing</em>. Thanks for considering this as you read.<br>
-                  (<a href="#a_respond" class="i_chat">comment</a> or <a href="mailto:cfrydlewicz@gmail.com" class="i_mail">contact me</a> to request an update about its subject matter)</p>
+            <?php if ( has_tag('old-post') ) : ?>
+              <div class="disclaimer_old-posts alert warning">
+                <div>
+                  <p class="f_small"><strong>This post contains outdated information.</strong> I only remove <em>harmful</em> content and try to keep old posts visible, even if they're <em>embarrassing</em>. Thanks for considering this as you read.<br>
+                    (<a href="#a_respond" class="i_chat">comment</a> or <a href="mailto:cfrydlewicz@gmail.com" class="i_mail">contact me</a> to request an update about its subject matter)</p>
+                </div>
               </div>
-            </div>
-          <?php elseif ( has_tag('unfinished') ) : ?>
-            <div class="disclaimer_unfinished alert warning">
-              <div>
-                <p class="f_small"><strong>This post is unfinished.</strong> I published it anyway because I believe it still has <em>some</em> value. On the bright side, it'll probably be short! 😇<br>
-                  (<a href="#a_respond" class="i_chat">comment</a> or <a href="mailto:cfrydlewicz@gmail.com" class="i_mail">contact me</a> to request an update about its subject matter)</p>
+            <?php elseif ( has_tag('unfinished') ) : ?>
+              <div class="disclaimer_unfinished alert warning">
+                <div>
+                  <p class="f_small"><strong>This post is unfinished.</strong> I published it anyway because I believe it still has <em>some</em> value. On the bright side, it'll probably be short! 😇<br>
+                    (<a href="#a_respond" class="i_chat">comment</a> or <a href="mailto:cfrydlewicz@gmail.com" class="i_mail">contact me</a> to request an update about its subject matter)</p>
+                </div>
               </div>
-            </div>
-          <?php endif; ?>
-
-          <?php the_content(); ?>
-        </div>
-      </section>
-
-      <footer id="a_end-of-article" class="entry-footer t_slides-up">
-        <div class="inner-wrapper">
-          <div class="thanks-header">Thanks for reading!</div>
-          <p class="post-categories">Categories: <?php the_category(', '); ?></p>
-          <p class="post-tags"><?php the_tags(); ?></p>
-          <p class="reverse-canary f_smallest">I have not been required to disclose data to the government under duress. If that changes and I'm able, I will remove this text. (last update: 2025-03-22)</p>
-        </div>
-      </footer>
-
-    </article>
-
-    <?php
-      echo $thisPostId[0];
-      $thisPostTitle = get_the_title($thisPostId);
-      $thisPostShortUrl = "https://corry.us/?p=".get_the_ID($thisPostId);
-      $thisPostCommentNum = get_comments_number($thisPostId);
-    ?>
-
-    <footer class="post-footer t_slides-up">
-
-      <?php if ( comments_open() ) : ?>
-        <section id="a_comments_top" class="comments-container t_slides-up">
-          <?php if ( $thisPostCommentNum > 0 ) : ?>
-            <div class="post-footer_header">Comments <span class="comments-count">(<?php echo $thisPostCommentNum; ?>)</span></div>
-          <?php endif; ?>
-          <?php comments_template(); ?>
-        </section>
-      <?php endif; ?>
-
-      <section class="discussion-container">
-        <div class="post-footer_header">Discuss</div>
-        <ul>
-          <?php if ( comments_open() ) : ?>
-            <?php if ( $thisPostCommentNum > 0 ) : ?>
-              <li><a class="i_arrow-up" href="#a_comments_top">View Comments (<?php echo $thisPostCommentNum; ?>)</a></li>
             <?php endif; ?>
-            <li><a class="i_chat" href="#a_respond">Leave a Comment</a></li>
-          <?php endif; ?>
-          <li><a class="i_bluesky" href="https://bsky.app/intent/compose?text=@corry.us%0A<?php echo $thisPostShortUrl; ?>" target="_blank" rel="noreferrer" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://bsky.app']);" style="white-space: nowrap;">Discuss on Bluesky</a></li>
-          <li><a class="i_heart" href="https://www.patreon.com/CorryFrydlewicz" target="_blank" rel="noreferrer" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://www.patreon.com']);">Patreon</a> for anyone who'd like to support my content and help me decide what to focus on!</li>
-        </ul>
-      </section>
 
-      <?php if ( !empty($thisPostShortUrl) ) : ?>
-        <section class="share-container">
-          <div class="post-footer_header">Share</div>
+            <?php the_content(); ?>
+          </div>
+        </section>
+
+        <footer id="a_end-of-article" class="entry-footer t_slides-up">
+          <div class="inner-wrapper">
+            <div class="thanks-header">Thanks for reading!</div>
+            <p class="post-categories">Categories: <?php the_category(', '); ?></p>
+            <p class="post-tags"><?php the_tags(); ?></p>
+            <p class="reverse-canary f_smallest">I have not been required to disclose data to the government under duress. If that changes and I'm able, I will remove this text. (last update: 2025-03-22)</p>
+          </div>
+        </footer>
+
+      </article>
+
+      <?php
+        echo $thisPostId[0];
+        $thisPostTitle = get_the_title($thisPostId);
+        $thisPostShortUrl = "https://corry.us/?p=".get_the_ID($thisPostId);
+        $thisPostCommentNum = get_comments_number($thisPostId);
+      ?>
+
+      <footer class="post-footer t_slides-up">
+
+        <?php if ( comments_open() ) : ?>
+          <section id="a_comments_top" class="comments-container t_slides-up">
+            <?php if ( $thisPostCommentNum > 0 ) : ?>
+              <div class="post-footer_header">Comments <span class="comments-count">(<?php echo $thisPostCommentNum; ?>)</span></div>
+            <?php endif; ?>
+            <?php comments_template(); ?>
+          </section>
+        <?php endif; ?>
+
+        <section class="discussion-container">
+          <div class="post-footer_header">Discuss</div>
           <ul>
-            <li><a class="i_mail" href="mailto:%20?subject=<?php echo $thisPostTitle; ?>&body=<?php echo $thisPostShortUrl; ?>" target="_blank">Share via Email</a></li>
-            <li><a class="i_bluesky" href="https://bsky.app/intent/compose?text=<?php echo $thisPostShortUrl; ?>" target="_blank" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://bsky.app']);">Share on Bluesky</a></li>
-            <li>URL: <a href="https://corry.us/<?php echo $post->post_name; ?>/" class="f_small">https://corry.us/<?php echo $post->post_name; ?>/</a></li>
-            <li>Short: <a href="<?php echo $thisPostShortUrl; ?>" class="f_small"><?php echo $thisPostShortUrl; ?></a></li>
+            <?php if ( comments_open() ) : ?>
+              <?php if ( $thisPostCommentNum > 0 ) : ?>
+                <li><a class="i_arrow-up" href="#a_comments_top">View Comments (<?php echo $thisPostCommentNum; ?>)</a></li>
+              <?php endif; ?>
+              <li><a class="i_chat" href="#a_respond">Leave a Comment</a></li>
+            <?php endif; ?>
+            <li><a class="i_bluesky" href="https://bsky.app/intent/compose?text=@corry.us%0A<?php echo $thisPostShortUrl; ?>" target="_blank" rel="noreferrer" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://bsky.app']);" style="white-space: nowrap;">Discuss on Bluesky</a></li>
+            <li><a class="i_heart" href="https://www.patreon.com/CorryFrydlewicz" target="_blank" rel="noreferrer" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://www.patreon.com']);">Patreon</a> for anyone who'd like to support my content and help me decide what to focus on!</li>
           </ul>
         </section>
-      <?php endif; ?>
 
-      <section class="follow-container">
-        <div class="post-footer_header">Follow This Blog</div>
-        <ul>
-          <li><a class="i_mail" href="/emailsubscribe?utm_source=post-footer" target="_blank">Email me</a> at <a href="mailto:cfrydlewicz@gmail.com" target="_blank">cfrydlewicz@gmail.com</a> to be notified when I post a new article.</li>
-          <li><a class="i_rss" href="/subscribe-to-corrys-blog-with-rss/" target="_blank">My RSS Feed</a> is the next best way to follow <em>new blog posts</em>.</li>
-          <li>I also share them on <a class="i_bluesky" href="/bluesky" rel="noreferrer" target="_blank" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://bsky.app']);">Bluesky</a>.</li>
-          <!--li><a class="i_heart" href="https://www.patreon.com/CorryFrydlewicz" target="_blank" rel="noreferrer" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://www.patreon.com']);">Patreon</a> is where I'll share more once I have a few subscribers.</li-->
-        </ul>
-      </section>
-
-      <section class="read-more-container">
-        <?php if ( is_active_sidebar( 'widget_single-post-footer1' ) ) : ?>
-          <div class="widget-area" role="complementary">
-            <?php dynamic_sidebar( 'widget_single-post-footer1' ); ?>
-          </div>
-        <?php else : ?>
-          <div class="widget-title">Read More</div>
-          <div><a href="/tag/favorite-post/">My Favorite Posts</a></div>
+        <?php if ( !empty($thisPostShortUrl) ) : ?>
+          <section class="share-container">
+            <div class="post-footer_header">Share</div>
+            <ul>
+              <li><a class="i_mail" href="mailto:%20?subject=<?php echo $thisPostTitle; ?>&body=<?php echo $thisPostShortUrl; ?>" target="_blank">Share via Email</a></li>
+              <li><a class="i_bluesky" href="https://bsky.app/intent/compose?text=<?php echo $thisPostShortUrl; ?>" target="_blank" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://bsky.app']);">Share on Bluesky</a></li>
+              <li>URL: <a href="https://corry.us/<?php echo $post->post_name; ?>/" class="f_small">https://corry.us/<?php echo $post->post_name; ?>/</a></li>
+              <li>Short: <a href="<?php echo $thisPostShortUrl; ?>" class="f_small"><?php echo $thisPostShortUrl; ?></a></li>
+            </ul>
+          </section>
         <?php endif; ?>
-      </section>
 
-    </footer>
+        <section class="follow-container">
+          <div class="post-footer_header">Follow This Blog</div>
+          <ul>
+            <li><a class="i_mail" href="/emailsubscribe?utm_source=post-footer" target="_blank">Email me</a> at <a href="mailto:cfrydlewicz@gmail.com" target="_blank">cfrydlewicz@gmail.com</a> to be notified when I post a new article.</li>
+            <li><a class="i_rss" href="/subscribe-to-corrys-blog-with-rss/" target="_blank">My RSS Feed</a> is the next best way to follow <em>new blog posts</em>.</li>
+            <li>I also share them on <a class="i_bluesky" href="/bluesky" rel="noreferrer" target="_blank" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://bsky.app']);">Bluesky</a>.</li>
+            <!--li><a class="i_heart" href="https://www.patreon.com/CorryFrydlewicz" target="_blank" rel="noreferrer" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://www.patreon.com']);">Patreon</a> is where I'll share more once I have a few subscribers.</li-->
+          </ul>
+        </section>
 
-  <?php endwhile; ?>
-</main>
+        <section class="read-more-container">
+          <?php if ( is_active_sidebar( 'widget_single-post-footer1' ) ) : ?>
+            <div class="widget-area" role="complementary">
+              <?php dynamic_sidebar( 'widget_single-post-footer1' ); ?>
+            </div>
+          <?php else : ?>
+            <div class="widget-title">Read More</div>
+            <div><a href="/tag/favorite-post/">My Favorite Posts</a></div>
+          <?php endif; ?>
+        </section>
 
+      </footer>
+
+    <?php endwhile; ?>
+  </main>
+
+  <aside class="sidebar">
+  </aside>
+
+</div><!--.main-columns-wrapper-->
 <?php get_footer(); ?>
