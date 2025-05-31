7523752b69b98ad3a6a2022ef2f1a15a30453311
Corry Frydlewicz <cfrydlewicz@gmail.com>
2025-05-31 01:13:22 -0700

    Reorganize Single Posts

--- single.php
+++ single.php
@@ -1,19 +1,23 @@
 <?php get_header(); ?>
 
-<?php if ( has_post_thumbnail() ) : ?>
-  <div class="stretched-bg" style="background-image: url('<?php the_post_thumbnail_url(); ?>');"><?php post_thumbnail(); ?></div>
-<?php endif; ?>
+<?php while ( have_posts() ) : ?>
+  <?php
+    the_post();
+    // store data for the post footer widgets
+    $thisPostId = get_the_ID();
+    echo $thisPostId[0];
+    $thisPostTitle = get_the_title($thisPostId);
+    $thisPostShortUrl = "https://corry.us/?p=".get_the_ID($thisPostId);
+    $thisPostCommentNum = get_comments_number($thisPostId);
+  ?>
 
-<div class="main-columns-wrapper inner-wrapper">
+  <?php if ( has_post_thumbnail() ) : ?>
+    <div class="stretched-bg" style="background-image: url('<?php the_post_thumbnail_url(); ?>');"><?php post_thumbnail(); ?></div>
+  <?php endif; ?>
 
-  <main id="a_skip-to-content"> 
-    <?php while ( have_posts() ) : ?>
-      <?php
-        the_post();
-        // store data for the post footer widgets
-        $thisPostId = get_the_ID();
-      ?>
+  <div class="main-columns-wrapper inner-wrapper">
 
+    <main id="a_skip-to-content"> 
       <article id="post-<?php the_ID(); ?>" <?php post_class('article-content'); ?>>
 
         <header class="entry-header">
@@ -51,52 +55,22 @@
           </div>
         </section>
 
-        <footer id="a_end-of-article" class="entry-footer t_slides-up">
-          <div class="inner-wrapper">
-            <div class="thanks-header">Thanks for reading!</div>
-            <p class="post-categories">Categories: <?php the_category(', '); ?></p>
-            <p class="post-tags"><?php the_tags(); ?></p>
-            <p class="reverse-canary f_smallest">I have not been required to disclose data to the government under duress. If that changes and I'm able, I will remove this text. (last update: 2025-03-22)</p>
-          </div>
-        </footer>
-
       </article>
+    </main>
 
-      <?php
-        echo $thisPostId[0];
-        $thisPostTitle = get_the_title($thisPostId);
-        $thisPostShortUrl = "https://corry.us/?p=".get_the_ID($thisPostId);
-        $thisPostCommentNum = get_comments_number($thisPostId);
-      ?>
+    <aside class="sidebar">
+      <div class="sidebar-inner sticky">
 
-      <footer class="post-footer t_slides-up">
-
-        <?php if ( comments_open() ) : ?>
-          <section id="a_comments_top" class="comments-container t_slides-up">
-            <?php if ( $thisPostCommentNum > 0 ) : ?>
-              <div class="post-footer_header">Comments <span class="comments-count">(<?php echo $thisPostCommentNum; ?>)</span></div>
-            <?php endif; ?>
-            <?php comments_template(); ?>
-          </section>
-        <?php endif; ?>
-
-        <section class="discussion-container">
-          <div class="post-footer_header">Discuss</div>
-          <ul>
-            <?php if ( comments_open() ) : ?>
-              <?php if ( $thisPostCommentNum > 0 ) : ?>
-                <li><a class="i_arrow-up" href="#a_comments_top">View Comments (<?php echo $thisPostCommentNum; ?>)</a></li>
-              <?php endif; ?>
-              <li><a class="i_chat" href="#a_respond">Leave a Comment</a></li>
-            <?php endif; ?>
-            <li><a class="i_bluesky" href="https://bsky.app/intent/compose?text=@corry.us%0A<?php echo $thisPostShortUrl; ?>" target="_blank" rel="noreferrer" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://bsky.app']);" style="white-space: nowrap;">Discuss on Bluesky</a></li>
-            <li><a class="i_heart" href="https://www.patreon.com/CorryFrydlewicz" target="_blank" rel="noreferrer" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://www.patreon.com']);">Patreon</a> for anyone who'd like to support my content and help me decide what to focus on!</li>
-          </ul>
+        <section class="post-meta">
+          <p><span class="word-count"><?php word_count(); ?> words</span><br>
+            <span class="post-date"><?php the_date(); ?></span></p>
+          <p class="post-categories">Categories: <?php the_category(', '); ?></p>
+          <p class="post-tags"><?php the_tags(); ?></p>
         </section>
 
         <?php if ( !empty($thisPostShortUrl) ) : ?>
           <section class="share-container">
-            <div class="post-footer_header">Share</div>
+            <div class="sidebar_header">Share This</div>
             <ul>
               <li><a class="i_mail" href="mailto:%20?subject=<?php echo $thisPostTitle; ?>&body=<?php echo $thisPostShortUrl; ?>" target="_blank">Share via Email</a></li>
               <li><a class="i_bluesky" href="https://bsky.app/intent/compose?text=<?php echo $thisPostShortUrl; ?>" target="_blank" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://bsky.app']);">Share on Bluesky</a></li>
@@ -107,7 +81,7 @@
         <?php endif; ?>
 
         <section class="follow-container">
-          <div class="post-footer_header">Follow This Blog</div>
+          <div class="sidebar_header">Follow This Blog</div>
           <ul>
             <li><a class="i_mail" href="/emailsubscribe?utm_source=post-footer" target="_blank">Email me</a> at <a href="mailto:cfrydlewicz@gmail.com" target="_blank">cfrydlewicz@gmail.com</a> to be notified when I post a new article.</li>
             <li><a class="i_rss" href="/subscribe-to-corrys-blog-with-rss/" target="_blank">My RSS Feed</a> is the next best way to follow <em>new blog posts</em>.</li>
@@ -116,34 +90,82 @@
           </ul>
         </section>
 
-        <section class="read-more-container">
-          <?php if ( is_active_sidebar( 'widget_single-post-footer1' ) ) : ?>
-            <div class="widget-area" role="complementary">
-              <?php dynamic_sidebar( 'widget_single-post-footer1' ); ?>
-            </div>
-          <?php else : ?>
-            <div class="widget-title">Read More</div>
-            <div><a href="/tag/favorite-post/">My Favorite Posts</a></div>
+        <?php if ( is_active_sidebar( 'widget_post-sidebar1' ) ) : ?>
+          <ul class="widget-area" role="complementary">
+            <?php dynamic_sidebar( 'widget_post-sidebar1' ); ?>
+          </ul>
+        <?php endif; ?>
+
+      </div>
+    </aside>
+
+  </div><!--.main-columns-wrapper-->
+
+  <footer id="a_end-of-article" class="entry-footer inner-wrapper">
+
+    <section class="thanks-for-reading">
+      <div class="thanks-header">Thanks for reading!</div>
+      <p class="reverse-canary f_smallest">I have not been required to disclose data to the government under duress. If that changes and I'm able, I will remove this text. (last update: 2025-03-22)</p>
+    </section>
+
+    <?php if ( comments_open() ) : ?>
+      <section id="a_comments_top" class="comments-container t_slides-up">
+        <?php if ( $thisPostCommentNum > 0 ) : ?>
+          <div class="post-footer_header">Comments <span class="comments-count">(<?php echo $thisPostCommentNum; ?>)</span></div>
+        <?php endif; ?>
+        <?php comments_template(); ?>
+      </section>
+    <?php endif; ?>
+
+    <section class="discussion-container">
+      <div class="post-footer_header">Discuss</div>
+      <ul>
+        <?php if ( comments_open() ) : ?>
+          <?php if ( $thisPostCommentNum > 0 ) : ?>
+            <li><a class="i_arrow-up" href="#a_comments_top">View Comments (<?php echo $thisPostCommentNum; ?>)</a></li>
           <?php endif; ?>
-        </section>
+          <li><a class="i_chat" href="#a_respond">Leave a Comment</a></li>
+        <?php endif; ?>
+        <li><a class="i_bluesky" href="https://bsky.app/intent/compose?text=@corry.us%0A<?php echo $thisPostShortUrl; ?>" target="_blank" rel="noreferrer" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://bsky.app']);" style="white-space: nowrap;">Discuss on Bluesky</a></li>
+        <li><a class="i_heart" href="https://www.patreon.com/CorryFrydlewicz" target="_blank" rel="noreferrer" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://www.patreon.com']);">Patreon</a> for anyone who'd like to support my content and help me decide what to focus on!</li>
+      </ul>
+    </section>
 
-      </footer>
+    <?php if ( !empty($thisPostShortUrl) ) : ?>
+      <section class="share-container">
+        <div class="post-footer_header">Share</div>
+        <ul>
+          <li><a class="i_mail" href="mailto:%20?subject=<?php echo $thisPostTitle; ?>&body=<?php echo $thisPostShortUrl; ?>" target="_blank">Share via Email</a></li>
+          <li><a class="i_bluesky" href="https://bsky.app/intent/compose?text=<?php echo $thisPostShortUrl; ?>" target="_blank" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://bsky.app']);">Share on Bluesky</a></li>
+          <li>URL: <a href="https://corry.us/<?php echo $post->post_name; ?>/" class="f_small">https://corry.us/<?php echo $post->post_name; ?>/</a></li>
+          <li>Short: <a href="<?php echo $thisPostShortUrl; ?>" class="f_small"><?php echo $thisPostShortUrl; ?></a></li>
+        </ul>
+      </section>
+    <?php endif; ?>
 
-    <?php endwhile; ?>
-  </main>
+    <section class="follow-container">
+      <div class="post-footer_header">Follow This Blog</div>
+      <ul>
+        <li><a class="i_mail" href="/emailsubscribe?utm_source=post-footer" target="_blank">Email me</a> at <a href="mailto:cfrydlewicz@gmail.com" target="_blank">cfrydlewicz@gmail.com</a> to be notified when I post a new article.</li>
+        <li><a class="i_rss" href="/subscribe-to-corrys-blog-with-rss/" target="_blank">My RSS Feed</a> is the next best way to follow <em>new blog posts</em>.</li>
+        <li>I also share them on <a class="i_bluesky" href="/bluesky" rel="noreferrer" target="_blank" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://bsky.app']);">Bluesky</a>.</li>
+        <!--li><a class="i_heart" href="https://www.patreon.com/CorryFrydlewicz" target="_blank" rel="noreferrer" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://www.patreon.com']);">Patreon</a> is where I'll share more once I have a few subscribers.</li-->
+      </ul>
+    </section>
 
-  <aside class="sidebar">
-    <div class="sidebar-inner">
-      <div class="post-meta"><span class="word-count"><?php word_count(); ?> words</span> <span class="separator"></span> <span class="post-date"><?php the_date(); ?></span></div>
-      <?php if ( is_active_sidebar( 'widget_post-sidebar1' ) ) : ?>
+    <section class="read-more-container">
+      <?php if ( is_active_sidebar( 'widget_single-post-footer1' ) ) : ?>
         <div class="widget-area" role="complementary">
-          <?php dynamic_sidebar( 'widget_post-sidebar1' ); ?>
+          <?php dynamic_sidebar( 'widget_single-post-footer1' ); ?>
         </div>
       <?php else : ?>
+        <div class="widget-title">Read More</div>
         <div><a href="/tag/favorite-post/">My Favorite Posts</a></div>
       <?php endif; ?>
-    </div>
-  </aside>
+    </section>
 
-</div><!--.main-columns-wrapper-->
+  </footer>
+
+<?php endwhile; ?>
+
 <?php get_footer(); ?>
