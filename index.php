961721ae963619bd774a01a721e736a7651bfb82
Corry Frydlewicz <cfrydlewicz@gmail.com>
2025-06-02 11:52:43 -0700

    Attempting to Fix Sidebar Recent Posts

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
           </footer>
         </article>
 
-      <?php endwhile; ?>
+      <?php endwhile; wp_reset_postdata(); ?>
 
       <div class="pagination-container inner-wrapper">
         <?php posts_nav_link('', 'Previous Page', 'Next Page'); ?>
@@ -96,62 +96,86 @@
   <aside class="sidebar" role="complementary">
     <div class="sidebar-inner">
 
-      <?php if ( !is_category(700) ) {
-        // latest Microblog posts
-        $args = array(
+      <?php if ( !is_category(700) ) : ?>
+        <?php $args = array(
+          // latest Microblog posts
           'post_type' => 'post',
+          'post_status' => 'publish',
           'cat' => '700',
           'posts_per_page' => 3,
           'order' => 'DESC',
           'orderby' => 'date'
         );
-      } else {
-        // latest non-Microblog posts
-        $args = array(
+        $recent_posts = new WP_Query($args);
+        if ($recent_posts->have_posts()) : ?>
+          <section class="latest-articles">
+            <h3>Latest Microblogs</h3>
+            <?php while ($recent_posts->have_posts()) : $recent_posts->the_post(); ?>
+              <article id="post-<?php the_ID(); ?>" <?php post_class('article-card'); ?>>
+                <header class="entry-header">
+                  <a href="<?php the_permalink(); ?>"><?php post_thumbnail(); ?></a>
+                  <div class="post-categories"><span class="u_visually-hidden">Categories:&nbsp;</span><?php the_category('<span class="separator"></span>'); ?></div>
+                </header>
+                <div class="entry-content">
+                  <div class="title-container">
+                    <div class="entry-title"><?php the_title( sprintf( '<a href="%s">', esc_url( get_permalink() ) ), '</a>' ); ?></div>
+                  </div>
+                  <div class="excerpt-container"><?php the_excerpt(); ?></div>
+                  <div class="cta-container">
+                    <a href="<?php the_permalink(); ?>"><button class="i_arrow-right--after">Read it Now!</button></a>
+                  </div>
+                </div><!--.entry-content-->
+                <footer class="entry-footer">
+                  <div class="post-stats"><span class="post-date"><?php the_date(); ?></span> <span class="separator"></span> <span class="word-count"><?php word_count(); ?> words</span></div>
+                </footer>
+              </article>
+            <?php endwhile; wp_reset_postdata(); ?>
+          </section>
+        <?php endif; ?>
+
+      <?php else : ?>
+        <?php $args = array(
+          // latest non-Microblog posts
           'post_type' => 'post',
           'cat' => '-700',
           'posts_per_page' => 3,
           'order' => 'DESC',
           'orderby' => 'date'
         );
-      }
-      $recent_posts = new WP_Query($args);
-      if ($recent_posts->have_posts()) : ?>
-        <section class="latest-articles">
-          <h3><?php if ( !is_category(700) ) : ?>
-              <span>Latest Articles</span>
-            <?php else : ?>
-              <span>Latest Microblogs</span>
-          <?php endif; ?></h3>
-          <?php while ($recent_posts->have_posts()) : $recent_posts->the_post(); ?>
-            <article id="post-<?php the_ID(); ?>" <?php post_class('article-card'); ?>>
-              <header class="entry-header">
-                <a href="<?php the_permalink(); ?>"><?php post_thumbnail(); ?></a>
-                <div class="post-categories"><span class="u_visually-hidden">Categories:&nbsp;</span><?php the_category('<span class="separator"></span>'); ?></div>
-              </header>
-              <div class="entry-content">
-                <div class="title-container">
-                  <div class="entry-title"><?php the_title( sprintf( '<a href="%s">', esc_url( get_permalink() ) ), '</a>' ); ?></div>
-                </div>
-                <div class="excerpt-container"><?php the_excerpt(); ?></div>
-                <div class="cta-container">
-                  <a href="<?php the_permalink(); ?>"><button class="i_arrow-right--after">Read it Now!</button></a>
-                </div>
-              </div><!--.entry-content-->
-              <footer class="entry-footer">
-                <div class="post-stats"><span class="post-date"><?php the_date(); ?></span> <span class="separator"></span> <span class="word-count"><?php word_count(); ?> words</span></div>
-              </footer>
-            </article>
-          <?php endwhile; wp_reset_postdata(); ?>
-        </section>
-      <?php else : ?>
-        <section class="latest-articles u_attn">
-          <h3>Links</h3>
-          <ul>
-            <li><a href="/tag/favorite-post/">My Favorite Posts</a></li>
-          </ul>
-        </section>
-      <?php endif; ?>
+        $recent_posts = new WP_Query($args);
+        if ($recent_posts->have_posts()) : ?>
+          <section class="latest-articles">
+            <h3>Latest Articles</h3>
+            <?php while ($recent_posts->have_posts()) : $recent_posts->the_post(); ?>
+              <article id="post-<?php the_ID(); ?>" <?php post_class('article-card'); ?>>
+                <header class="entry-header">
+                  <a href="<?php the_permalink(); ?>"><?php post_thumbnail(); ?></a>
+                  <div class="post-categories"><span class="u_visually-hidden">Categories:&nbsp;</span><?php the_category('<span class="separator"></span>'); ?></div>
+                </header>
+                <div class="entry-content">
+                  <div class="title-container">
+                    <div class="entry-title"><?php the_title( sprintf( '<a href="%s">', esc_url( get_permalink() ) ), '</a>' ); ?></div>
+                  </div>
+                  <div class="excerpt-container"><?php the_excerpt(); ?></div>
+                  <div class="cta-container">
+                    <a href="<?php the_permalink(); ?>"><button class="i_arrow-right--after">Read it Now!</button></a>
+                  </div>
+                </div><!--.entry-content-->
+                <footer class="entry-footer">
+                  <div class="post-stats"><span class="post-date"><?php the_date(); ?></span> <span class="separator"></span> <span class="word-count"><?php word_count(); ?> words</span></div>
+                </footer>
+              </article>
+            <?php endwhile; wp_reset_postdata(); ?>
+          </section>
+        <?php else : ?>
+          <section class="latest-articles u_attn">
+            <h3>Links</h3>
+            <ul>
+              <li><a href="/tag/favorite-post/">My Favorite Posts</a></li>
+            </ul>
+          </section>
+        <?php endif; // have_posts ?>
+      <?php endif; // Microblog or not ?>
 
       <?php if ( is_active_sidebar( 'widget_listing-sidebar1' ) ) : ?>
         <ul class="widget-area">
