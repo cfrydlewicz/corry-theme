8565cbd485c539894000223eddf59b5bc411b3a8
Corry Frydlewicz <cfrydlewicz@gmail.com>
2025-06-05 14:58:07 -0700

    Update 404 Card w/ Standard Card Template

--- index.php
+++ index.php
@@ -57,35 +57,33 @@
 
       <article id="error404" class="article-card u_attn">
         <header class="entry-header">
-          <div class="inner-wrapper--at-lg">
-            <a href="/">
-              <picture>
-                <source media="(min-width: 769px)" srcset="<?php bloginfo('template_url');?>/assets/images/404_1024x512.jpg">
-                <img src="<?php bloginfo('template_url');?>/assets/images/404_512x512.jpg" alt="Mega Man enters a boss stage only to find a 404 Boss Not found instead">
-              </picture>
-            </a>
-            <div class="title-container">
-              <?php if ( is_search() ) : ?>
-                <h1 id="sticky-title" class="entry-title">Sorry, No Results Found</h1>
-              <?php else : ?>
-                <h2 class="entry-title"><a href="/">Oops! Wrong turn!</a></h2>
-              <?php endif; ?>
-            </div>
-          </div>
+          <a href="/">
+            <picture>
+              <source media="(min-width: 769px)" srcset="<?php bloginfo('template_url');?>/assets/images/404_1024x512.jpg">
+              <img src="<?php bloginfo('template_url');?>/assets/images/404_512x512.jpg" alt="Mega Man enters a boss stage only to find a 404 Boss Not found instead">
+            </picture>
+          </a>
+          <div class="post-categories"><span class="u_visually-hidden">Categories:&nbsp;</span>404 Page</div>
         </header>
-        <div class="entry-content">
-          <div class="inner-wrapper">
-            <div class="excerpt-container">
-              <p>I couldn't find what you're looking for. Please try searching in another way or check out my homepage. If you think something's broken and have a minute to <a href="mailto:cfrydlewicz@gmail.com">let me know</a>, I'd appreciate it.</p>
-              <a href="/" class="i_arrow-right--after">Latest Posts</a>
-            </div>
+        <div class="entry-content sp_horizontal-padding">
+          <div class="title-container">
+            <?php if ( is_search() ) : ?>
+              <h1 id="sticky-title" class="entry-title">Sorry, No Results Found</h1>
+            <?php else : ?>
+              <h2 class="entry-title"><a href="/">Oops! Wrong turn!</a></h2>
+            <?php endif; ?>
+          </div>
+          <div class="excerpt-container">
+            <p>I couldn't find what you're looking for. Please try searching in another way or check out my homepage. If you think something's broken and have a minute to <a href="mailto:cfrydlewicz@gmail.com" class="u_nowrap">let me know</a>, I'd appreciate it.</p>
+          </div>
+          <div class="cta-container">
+            <a href="/">
+              <button class="i_arrow-right--after">Latest Posts</button>
+            </a>
           </div>
         </div><!--.entry-content-->
-        <footer class="entry-footer">
-          <div class="inner-wrapper">
-            <div class="post-stats"><span class="post-date">20XX</span> <span class="separator"></span> <span class="word-count">0 words</span></div>
-            <div class="post-categories"><span class="u_visually-hidden">Categories:&nbsp;</span>404 Page</div>
-          </div>
+        <footer class="entry-footer sp_horizontal-padding">
+          <div class="post-stats"><span class="post-date">20XX</span> <span class="separator"></span> <span class="word-count">0 words</span></div>
         </footer>
       </article>
 
