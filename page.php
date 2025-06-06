2898c80675730d85448a9155b936ff6f404bf0f8
Corry Frydlewicz <cfrydlewicz@gmail.com>
2025-06-05 23:12:39 -0700

    Page Template Updates

--- page.php
+++ page.php
@@ -1,32 +1,26 @@
 <?php get_header(); ?>
+<?php while ( have_posts() ) : ?>
+  <?php
+    the_post();
+    // store data for the post footer widgets
+    $thisPostId = get_the_ID();
+    echo $thisPostId[0];
+    $thisPostTitle = get_the_title($thisPostId);
+    $thisPostShortUrl = "https://corry.us/?p=".get_the_ID($thisPostId);
+    $thisPostCommentNum = get_comments_number($thisPostId);
+  ?>
 
-<main id="a_skip-to-content">
-  <?php while ( have_posts() ) : ?>
-    <?php
-      the_post();
-      // store data for the post footer widgets
-      $thisPostId = get_the_ID();
-    ?>
-
+  <main id="a_skip-to-content">
     <article id="post-<?php the_ID(); ?>" <?php post_class('article-content'); ?>>
 
       <header class="entry-header">
-        <div class="stretched-bg" style="background-image: url('<?php
-          if ( has_post_thumbnail() ) {
-            the_post_thumbnail_url();
-          } else if ( is_category(700) ) {
-            echo "/wp-content/themes/corry/assets/images/thumbnail-microblog.jpg";
-          } else {
-            echo "/wp-content/themes/corry/assets/images/thumbnail-default.jpg";
-          }
-        ?>');"></div>
-        <div class="inner-wrapper--at-lg">
-          <a href="<?php the_post_thumbnail_url(); ?>">
-            <?php post_thumbnail(); ?>
-          </a>
-          <div class="title-container">
-            <h1 id="sticky-title" class="entry-title"><?php the_title(); ?></h1>
-          </div>
+        <?php if ( has_post_thumbnail() ) : ?>
+          <?php post_thumbnail(); ?>
+        <?php else : ?>
+          <img alt="default thumbnail" src="/wp-content/themes/corry/assets/images/thumbnail-default.jpg" class="u_attn">
+        <?php endif; ?>
+        <div class="title-container">
+          <h1 id="sticky-title" class="entry-title"><?php the_title(); ?></h1>
         </div>
       </header>
 
@@ -36,78 +30,69 @@
         </div>
       </section>
 
-      <footer id="a_end-of-article" class="entry-footer t_slides-up">
-        <div class="inner-wrapper">
-          <div class="thanks-header">Thanks for reading!</div>
-        </div>
-      </footer>
-
     </article>
+  </main>
 
-    <?php
-      echo $thisPostId[0];
-      $thisPostTitle = get_the_title($thisPostId);
-      $thisPostShortUrl = "https://corry.us/?p=".get_the_ID($thisPostId);
-      $thisPostCommentNum = get_comments_number($thisPostId);
-    ?>
+  <footer id="a_end-of-article" class="post-footer inner-wrapper">
 
-    <footer class="post-footer t_slides-up">
-
-      <?php if ( comments_open() ) : ?>
-        <section id="a_comments_top" class="comments-container t_slides-up">
-          <?php if ( $thisPostCommentNum > 0 ) : ?>
-            <div class="post-footer_header">Comments <span class="comments-count">(<?php echo $thisPostCommentNum; ?>)</span></div>
-          <?php endif; ?>
-          <?php comments_template(); ?>
-        </section>
-
-        <section class="discussion-container">
-          <div class="post-footer_header">Discuss</div>
-          <ul>
+    <section class="thanks-for-reading">
+      <div class="thanks-main">
+        <div class="thanks-header">Thanks for reading!</div>
+        <div class="discuss">
+          <span class="label">Discuss: </span>
+          <?php if ( comments_open() ) : ?>
+            <a class="i_chat" href="#a_end-of-article" title="Leave a Comment"><span class="u_visually-hidden">Leave a Comment</span></a>
             <?php if ( $thisPostCommentNum > 0 ) : ?>
-              <li><a class="i_arrow-up" href="#a_comments_top">View Comments (<?php echo $thisPostCommentNum; ?>)</a></li>
+              <span class="comments-count"><?php echo $thisPostCommentNum; ?><span class="u_visually-hidden"> Comments</span></span>
             <?php endif; ?>
-            <li><a class="i_chat" href="#a_respond">Leave a Comment</a></li>
-            <li><a class="i_bluesky" href="https://bsky.app/intent/compose?text=@corry.us%0A<?php echo $thisPostShortUrl; ?>" target="_blank" rel="noreferrer" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://bsky.app']);" style="white-space: nowrap;">Discuss on Bluesky</a></li>
-            <li><a class="i_heart" href="https://www.patreon.com/CorryFrydlewicz" target="_blank" rel="noreferrer" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://www.patreon.com']);">Patreon</a> for anyone who'd like to support my content and help me decide what to focus on!</li>
-          </ul>
-        </section>
-      <?php endif; ?>
+          <?php endif; ?>
+          <a class="i_bluesky" href="https://bsky.app/intent/compose?text=@corry.us%0A<?php echo $thisPostShortUrl; ?>" target="_blank" rel="noreferrer" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://bsky.app']);" title="Discuss on Bluesky"><span class="u_visually-hidden">Discuss on Bluesky</span></a>
+        </div>
+        <?php if ( !empty($thisPostShortUrl) ) : ?>
+          <div class="share">
+            <span class="label">Share: </span>
+            <a class="i_mail" href="mailto:%20?subject=<?php echo $thisPostTitle; ?>&body=<?php echo $thisPostShortUrl; ?>" target="_blank" title="Share via Email"><span class="u_visually-hidden">Share via Email</span></a>
+            <a class="i_bluesky" href="https://bsky.app/intent/compose?text=<?php echo $thisPostShortUrl; ?>" target="_blank" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://bsky.app']);" title="Share on Bluesky"><span class="u_visually-hidden">Share on Bluesky</span></a><br>
+            <a class="f_small" href="https://corry.us/<?php echo $post->post_name; ?>">corry.us/<?php echo $post->post_name; ?></a>
+          </div>
+        <?php endif; ?>
+      </div>
+      <div class="post-stats f_small">
+        <p class="post-categories">Categories: <?php the_category(', '); ?></p>
+        <p class="post-tags"><?php the_tags(); ?></p>
+      </div>
+    </section>
 
-      <?php if ( !empty($thisPostShortUrl) ) : ?>
-        <section class="share-container">
-          <div class="post-footer_header">Share</div>
-          <ul>
-            <li><a class="i_mail" href="mailto:%20?subject=<?php echo $thisPostTitle; ?>&body=<?php echo $thisPostShortUrl; ?>" target="_blank">Share via Email</a></li>
-            <li><a class="i_bluesky" href="https://bsky.app/intent/compose?text=<?php echo $thisPostShortUrl; ?>" target="_blank" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://bsky.app']);">Share on Bluesky</a></li>
-            <li>URL: <a href="https://corry.us/<?php echo $post->post_name; ?>/" class="f_small">https://corry.us/<?php echo $post->post_name; ?>/</a></li>
-          </ul>
-        </section>
-      <?php endif; ?>
-
-      <section class="follow-container">
-        <div class="post-footer_header">Follow Me</div>
-        <ul>
-          <li><a class="i_mail" href="/emailsubscribe?utm_source=post-footer" target="_blank">Email me</a> at <a href="mailto:cfrydlewicz@gmail.com" target="_blank">cfrydlewicz@gmail.com</a> to be notified when I post a new article.</li>
-          <li><a class="i_rss" href="/subscribe-to-corrys-blog-with-rss/" target="_blank">My RSS Feed</a> is the next best way to follow <em>new blog posts</em>.</li>
-          <li>I also share them on <a class="i_bluesky" href="/bluesky" rel="noreferrer" target="_blank" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://bsky.app']);">Bluesky</a>.</li>
-          <!--li><a class="i_heart" href="https://www.patreon.com/CorryFrydlewicz" target="_blank" rel="noreferrer" onclick="javascript:_gaq.push(['_trackEvent','outbound-widget','https://www.patreon.com']);">Patreon</a> is where I'll share more once I have a few subscribers.</li-->
-        </ul>
+    <?php if ( comments_open() ) : ?>
+      <section id="a_comments_top" class="comments-container t_slides-up">
+        <?php if ( $thisPostCommentNum > 0 ) : ?>
+          <div class="post-footer_header">Comments <span class="comments-count">(<?php echo $thisPostCommentNum; ?>)</span></div>
+        <?php endif; ?>
+        <?php comments_template(); ?>
       </section>
+    <?php endif; ?>
 
-      <section class="read-more-container">
-        <div class="post-footer_header">Read More</div>
-        <ul>
-          <li><a href="/about/">About Me</a></li>
-          <li><a href="/tag/favorite-post/">My Favorite Posts</a></li>
-          <li><a href="/">All Blog Posts</a></li>
-          <li><a href="/category/microblog/">Microblog</a></li>
+    <section class="read-more-container" role="complementary">
+      <?php if ( is_active_sidebar( 'widget_single-post-footer1' ) ) : ?>
+        <ul class="widget-area">
+          <?php dynamic_sidebar( 'widget_single-post-footer1' ); ?>
         </ul>
-      </section>
+      <?php else : ?>
+        <ul class="u_attn">
+          <li>
+            <div class="post-footer_header widget-title">Continue Reading</div>
+            <div>
+              <ul>
+                <li><a href="/tag/favorite-post/">My Favorite Posts</a></li>
+                <li><a href="/top-tens/">My Top Ten <em>Everything</em></a></li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      <?php endif; ?>
+    </section>
 
-    </footer>
+  </footer>
 
-  <?php endwhile; ?>
-</main>
-
+<?php endwhile; wp_reset_postdata(); ?>
 <?php get_footer(); ?>
