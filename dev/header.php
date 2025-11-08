33ec37b3f1abb3c996f033ab6d4c882ce84319de
Corry Frydlewicz <cfrydlewicz@gmail.com>
2025-11-08 08:43:33 -0800

    First Draft Animosity Styles

--- dev/header.php
+++ dev/header.php
@@ -75,6 +75,7 @@ $saveData = (isset($_SERVER["HTTP_SAVE_DATA"]) && stristr($_SERVER["HTTP_SAVE_DA
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,800;1,300;1,400;1,500&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Press+Start+2P&display=swap" rel="stylesheet">
 
   <style>/* critical inline CSS <!--THEME VERSION--> */
 <!--INJECT CRITICAL INLINE CSS HERE -->
